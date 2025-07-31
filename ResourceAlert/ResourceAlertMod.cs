cb9ee10c2f184d18c068b98703b5b29fa834edcc
Deep <lukaszkulwicki@gmail.com>
2025-07-31 16:12:06 +0200

    bug related to multiple instantion of loaded list fixed

--- ResourceAlert/ResourceAlertMod.cs
+++ ResourceAlert/ResourceAlertMod.cs
@@ -1,59 +1,58 @@
 ﻿using System.Collections.Generic;
-using UnityEngine;
+using System.Linq;
 using Verse;
-using RimWorld;
-using Verse.Sound;
 
 namespace ResourceAlert
 {
-    public class ResourceAlertModSettings : ModSettings
-    {
-        public override void ExposeData()
-        {
-            base.ExposeData();
-            Scribe_Collections.Look(
-                ref Alert_LowResource.alertableResources,
-                "Deep_ResourceAlert_alertableResources",
-                LookMode.Def, LookMode.Value);
-            Scribe_Collections.Look(
-                ref Alert_LowResource.alertableCategories,
-                "Deep_ResourceAlert_alertableCategories",
-                LookMode.Def, LookMode.Value);
+	/// <summary>
+	/// Centralizes loading/saving of alertable resources and categories.
+	/// Remove any duplicate ResourceAlertSettings classes to avoid conflicts.
+	/// </summary>
+	public class ResourceAlertModSettings : ModSettings
+	{
 
-            if (Alert_LowResource.alertableResources == null)
-                Alert_LowResource.alertableResources = new Dictionary<ThingDef, int>();
-            if (Alert_LowResource.alertableCategories == null)
-                Alert_LowResource.alertableCategories = new Dictionary<ThingCategoryDef, int>();
-        }
-    }
-
-    public class ResourceAlertMod : Mod
-    {
-        private ResourceAlertModSettings settings;
-
-        public ResourceAlertMod(ModContentPack content) : base(content)
-        {
-            this.settings = GetSettings<ResourceAlertModSettings>();
-        }
-
-        public override string SettingsCategory()
-        {
-            return "Deep Resource Alert";
-        }
-
-		public override void DoSettingsWindowContents(Rect inRect)
+		public override void ExposeData()
 		{
-			Listing_Standard listing = new Listing_Standard();
-			listing.Begin(inRect);
+			base.ExposeData();
 
-			if (listing.ButtonText("Show Tracked Resources"))
+			// Load or save the list of alertable resources
+			Scribe_Collections.Look(
+				ref Alert_LowResource.alertableResources,
+				"Deep_ResourceAlert_alertableResources",
+				LookMode.Def,
+				LookMode.Value);
+
+			// Load or save the list of alertable categories
+			Scribe_Collections.Look(
+				ref Alert_LowResource.alertableCategories,
+				"Deep_ResourceAlert_alertableCategories",
+				LookMode.Def,
+				LookMode.Value);
+
+			// Initialize dictionaries if missing
+			if (Alert_LowResource.alertableResources == null)
+				Alert_LowResource.alertableResources = new Dictionary<ThingDef, int>();
+			else
 			{
-				SoundDefOf.Click.PlayOneShot(SoundInfo.OnCamera());
-				Find.WindowStack.Add(new TrackedResourcesWindow());
+				// Remove entries with missing ThingDef (null keys)
+				var nullKeys = Alert_LowResource.alertableResources.Keys
+					.Where(k => k == null)
+					.ToList();
+				foreach (var key in nullKeys)
+					Alert_LowResource.alertableResources.Remove(key);
 			}
 
-			listing.End();
+			if (Alert_LowResource.alertableCategories == null)
+				Alert_LowResource.alertableCategories = new Dictionary<ThingCategoryDef, int>();
+			else
+			{
+				// Remove entries with missing ThingCategoryDef (null keys)
+				var nullCatKeys = Alert_LowResource.alertableCategories.Keys
+					.Where(k => k == null)
+					.ToList();
+				foreach (var key in nullCatKeys)
+					Alert_LowResource.alertableCategories.Remove(key);
+			}
 		}
-
 	}
 }
--- ResourceAlert/ResourceChecker.cs
+++ ResourceAlert/ResourceChecker.cs
@@ -8,50 +8,50 @@ using Verse;
 
 namespace ResourceAlert
 {
-    static class ResourceChecker
-    {
+	static class ResourceChecker
+	{
 
-        public static void AddAlertableResource(ThingDef resource, int alert_amount = 100)
-        {
-            Log.Message("Add Alertable Resource: " + resource.LabelCap + "amount: " + alert_amount);
-            if (resource != null)
-            {
-                if (Alert_LowResource.alertableResources.ContainsKey(resource))
-                {
-                    Alert_LowResource.alertableResources.Remove(resource);
-                    Messages.Message("Deep_ResourceAlert_ResourceChecker_KeyReplaced".Translate(resource.LabelCap, alert_amount), MessageTypeDefOf.NeutralEvent);
-                }
-                Alert_LowResource.alertableResources.Add(resource, alert_amount);
-            }
-        }
+		public static void AddAlertableResource(ThingDef resource, int alert_amount = 100)
+		{
+			Log.Message("Add Alertable Resource: " + resource.LabelCap + "amount: " + alert_amount);
+			if (resource != null)
+			{
+				if (Alert_LowResource.alertableResources.ContainsKey(resource))
+				{
+					Alert_LowResource.alertableResources.Remove(resource);
+					Messages.Message("Deep_ResourceAlert_ResourceChecker_KeyReplaced".Translate(resource.LabelCap, alert_amount), MessageTypeDefOf.NeutralEvent);
+				}
+				Alert_LowResource.alertableResources.Add(resource, alert_amount);
+			}
+		}
 
-        public static void AddAlertableCategory(ThingCategoryDef resource, int alert_amount = 100)
-        {
-            Log.Message("Add Alertable Category: " + resource.LabelCap + "amount: " + alert_amount);
-            if (resource != null)
-            {
-                if (Alert_LowResource.alertableCategories.ContainsKey(resource))
-                {
-                    Alert_LowResource.alertableCategories.Remove(resource);
-                    Messages.Message("Deep_ResourceAlert_ResourceChecker_KeyReplaced".Translate(resource.LabelCap, alert_amount), MessageTypeDefOf.NeutralEvent);
-                }
-                Alert_LowResource.alertableCategories.Add(resource, alert_amount);
-            }
-        }
+		public static void AddAlertableCategory(ThingCategoryDef resource, int alert_amount = 100)
+		{
+			Log.Message("Add Alertable Category: " + resource.LabelCap + "amount: " + alert_amount);
+			if (resource != null)
+			{
+				if (Alert_LowResource.alertableCategories.ContainsKey(resource))
+				{
+					Alert_LowResource.alertableCategories.Remove(resource);
+					Messages.Message("Deep_ResourceAlert_ResourceChecker_KeyReplaced".Translate(resource.LabelCap, alert_amount), MessageTypeDefOf.NeutralEvent);
+				}
+				Alert_LowResource.alertableCategories.Add(resource, alert_amount);
+			}
+		}
 
-        public static void RemoveAlertableResource(ThingDef resource)
-        {
-            if (resource != null)
-            {
-                Alert_LowResource.alertableResources.Remove(resource);
-            }
-        }
-        public static void RemoveAlertableCategory(ThingCategoryDef resource)
-        {
-            if (resource != null)
-            {
-                Alert_LowResource.alertableCategories.Remove(resource);
-            }
-        }
-    }
+		public static void RemoveAlertableResource(ThingDef resource)
+		{
+			if (resource != null)
+			{
+				Alert_LowResource.alertableResources.Remove(resource);
+			}
+		}
+		public static void RemoveAlertableCategory(ThingCategoryDef resource)
+		{
+			if (resource != null)
+			{
+				Alert_LowResource.alertableCategories.Remove(resource);
+			}
+		}
+	}
 }
