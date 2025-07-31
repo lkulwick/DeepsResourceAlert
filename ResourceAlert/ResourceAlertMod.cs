ebf810de155e7fdc6b3349580a3fb53eeecc14c1
Deep <lukaszkulwicki@gmail.com>
2025-07-31 16:51:47 +0200

    Revert "bug related to multiple instantion of loaded list fixed"
    This reverts commit cb9ee10c2f184d18c068b98703b5b29fa834edcc.

--- ResourceAlert/ResourceAlertMod.cs
+++ ResourceAlert/ResourceAlertMod.cs
@@ -1,58 +1,59 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
+using UnityEngine;
 using Verse;
+using RimWorld;
+using Verse.Sound;
 
 namespace ResourceAlert
 {
-	/// <summary>
-	/// Centralizes loading/saving of alertable resources and categories.
-	/// Remove any duplicate ResourceAlertSettings classes to avoid conflicts.
-	/// </summary>
-	public class ResourceAlertModSettings : ModSettings
-	{
+    public class ResourceAlertModSettings : ModSettings
+    {
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Collections.Look(
+                ref Alert_LowResource.alertableResources,
+                "Deep_ResourceAlert_alertableResources",
+                LookMode.Def, LookMode.Value);
+            Scribe_Collections.Look(
+                ref Alert_LowResource.alertableCategories,
+                "Deep_ResourceAlert_alertableCategories",
+                LookMode.Def, LookMode.Value);
 
-		public override void ExposeData()
+            if (Alert_LowResource.alertableResources == null)
+                Alert_LowResource.alertableResources = new Dictionary<ThingDef, int>();
+            if (Alert_LowResource.alertableCategories == null)
+                Alert_LowResource.alertableCategories = new Dictionary<ThingCategoryDef, int>();
+        }
+    }
+
+    public class ResourceAlertMod : Mod
+    {
+        private ResourceAlertModSettings settings;
+
+        public ResourceAlertMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<ResourceAlertModSettings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Deep Resource Alert";
+        }
+
+		public override void DoSettingsWindowContents(Rect inRect)
 		{
-			base.ExposeData();
+			Listing_Standard listing = new Listing_Standard();
+			listing.Begin(inRect);
 
-			// Load or save the list of alertable resources
-			Scribe_Collections.Look(
-				ref Alert_LowResource.alertableResources,
-				"Deep_ResourceAlert_alertableResources",
-				LookMode.Def,
-				LookMode.Value);
-
-			// Load or save the list of alertable categories
-			Scribe_Collections.Look(
-				ref Alert_LowResource.alertableCategories,
-				"Deep_ResourceAlert_alertableCategories",
-				LookMode.Def,
-				LookMode.Value);
-
-			// Initialize dictionaries if missing
-			if (Alert_LowResource.alertableResources == null)
-				Alert_LowResource.alertableResources = new Dictionary<ThingDef, int>();
-			else
+			if (listing.ButtonText("Show Tracked Resources"))
 			{
-				// Remove entries with missing ThingDef (null keys)
-				var nullKeys = Alert_LowResource.alertableResources.Keys
-					.Where(k => k == null)
-					.ToList();
-				foreach (var key in nullKeys)
-					Alert_LowResource.alertableResources.Remove(key);
+				SoundDefOf.Click.PlayOneShot(SoundInfo.OnCamera());
+				Find.WindowStack.Add(new TrackedResourcesWindow());
 			}
 
-			if (Alert_LowResource.alertableCategories == null)
-				Alert_LowResource.alertableCategories = new Dictionary<ThingCategoryDef, int>();
-			else
-			{
-				// Remove entries with missing ThingCategoryDef (null keys)
-				var nullCatKeys = Alert_LowResource.alertableCategories.Keys
-					.Where(k => k == null)
-					.ToList();
-				foreach (var key in nullCatKeys)
-					Alert_LowResource.alertableCategories.Remove(key);
-			}
+			listing.End();
 		}
+
 	}
 }
